c0b9b978b9def30c65a13e40259fde89685c9fd8
kobarin8 <rinchofu2@gmail.com>
2018-07-30 10:51:54 +0900

    最終アップロード

---
+++ script.js
@@ -0,0 +1,20 @@
+  
+    $(document).ready(function(){
+      $('.abc').bxSlider({
+        auto:true,
+        mode:'fade',
+        speed:400,
+        slidewidth:400 });
+        
+    });
+
+     
+ $(function() {
+    $('.abc').slick(
+    
+        
+        {autoplay:true,}
+    
+    
+    );
+});
